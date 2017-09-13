6acf60315b6ca488072375063a48257dd54ea3cd
Soumik Patel <patel.soumik4@gmail.com>
2017-09-13 21:10:26 +1200

    Basic JS text change

---
+++ practice-web/index.js
@@ -0,0 +1,13 @@
+﻿function changeIt() {
+
+    var input = document.getElementById('firstTextBox').value;
+
+    if (input == 0) {
+        alert('Please type the new heading.');
+        return;
+    }
+
+    var title = document.getElementById('title');
+    title.innerHTML = input;
+
+}
